d76b8a4ae17e42fa352d9b7a7824af3fc1a87db3
Anurag Hom Roy <homroyanurag@gmail.com>
2026-02-11 00:21:24 +0530

    Mistake rectified

--- GeorgeandAccommodation.cpp
+++ GeorgeandAccommodation.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 using namespace std;
-int main(){
 
-    int n,r=0;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        int p,q;
-        cin>>p>>q;
-        if(p<q){
+int main() {
+    int n, r = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int p, q;
+        cin >> p >> q;
+        if (q - p >= 2) {
             r++;
         }
     }
-    cout<<r;
 
+    cout << r;
     return 0;
 }
