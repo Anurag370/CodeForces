44173f215a81649b144c0991799bb7fb77a3ad4e
Anurag Hom Roy <homroyanurag@gmail.com>
2026-02-10 08:47:03 +0530

    10/02/2026

--- BeautifulYear.cpp
+++ BeautifulYear.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <set>
 using namespace std;
-int main(){
 
-    
+int main() {
+    int year;
+    cin >> year;
+
+    while (true) {
+        year++;
+        int temp = year;
+        set<int> digits;
+
+        while (temp > 0) {
+            digits.insert(temp % 10);
+            temp /= 10;
+        }
+
+        if (digits.size() == 4) {
+            cout << year;
+            break;
+        }
+    }
+
     return 0;
 }
